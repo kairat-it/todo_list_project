07cb9c246fdf8177936d706e5d90a2eba7539494
Kairat <ischenbaew@gmai.com>
2024-06-24 16:15:15 +0200

    Добавлен views.py и forms.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
