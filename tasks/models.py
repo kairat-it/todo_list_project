6adcb995f00b8f59b19d311a73af23f46c899bb8
Kairat <ischenbaew@gmai.com>
2024-06-24 16:11:55 +0200

    создан admin.py и Task а models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано'),
+    ]
+
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.description
