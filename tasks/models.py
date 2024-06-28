2c1415a63b59c9730983415e0ff1e0e1db6bd415
Kairat <ischenbaew@gmai.com>
2024-06-28 11:51:12 +0200

    исправил путь к страницам

--- tasks/models.py
+++ tasks/models.py
@@ -12,5 +12,6 @@ class Task(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     due_date = models.DateField(null=True, blank=True)
 
+
     def __str__(self):
         return self.description
