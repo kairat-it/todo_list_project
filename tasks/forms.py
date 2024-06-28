4d204d8eb3a6b0227efdde9113c47795a8950766
Kairat <ischenbaew@gmai.com>
2024-06-28 11:57:55 +0200

    Обновил TaskForm для поддержки нового поля

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,9 +5,10 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['description', 'status', 'due_date']
+        fields = ['description', 'status', 'due_date', 'detail_views']
         labels = {
             'description': 'Описание',
             'status': 'Статус',
             'due_date': 'Дата выполнения',
+            'detail_views': 'Подробнее'
         }
